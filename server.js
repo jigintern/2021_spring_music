278854b1ce85caf962b8e8d67907d53f31a81fa7
harukihirota <haruki120306b@icloud.com>
2021-03-29 18:51:05 +0900

    delete

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 import { Server } from "https://js.sabae.cc/Server.js";
+
+const data = [
+    {
+        music: "ドレミ"
+    }
+]
+
 class MyServer extends Server {
-    api(path) {
+    api(path, req) {
+        if (path == "/api/get"){
+            return data;
+        }
+        else if (path == "/api/add"){
+            data.push(req);
+            Deno.writeTextFileSync("data.json", JSON.stringify(data));
+            return "OK";
+        }
         return {name: "jigintern", path: path};
     }
 }
